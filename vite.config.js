931277c1b323dcaf570e3116e651dbec15cb68ab
3CFStudios <70796181+3CFStudios@users.noreply.github.com>
2026-02-01 21:44:45 +0530

    Include account page in Vite build

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         admin: resolve(__dirname, 'admin/index.html'),
         login: resolve(__dirname, 'login.html'),
+        account: resolve(__dirname, 'account.html'),
       }
     },
     outDir: 'dist'
